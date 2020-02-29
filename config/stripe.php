eb951b8a1621b78c9bf664a214e55c8ecaf548c0
subscriber <hassapasha@yahoo.com>
2020-02-29 15:15:48 +0200

    setup cashier payment

--- config/stripe.php
+++ config/stripe.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-  'published_key'   => 'pk_test_UcWltYRizYSueriqGDATuYBv005OtFUicc',
-  'secret_key'      => 'sk_test_fFDHy14ML0ox3sNgnibiNAJU006wW4MZR5'
+  'published_key'   => env('STRIPE_KEY'),
+  'secret_key'      => env('STRIPE_SECRET')
 ];
